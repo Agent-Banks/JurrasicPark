33a6af49739dcb2fa6269c7abc6a860a0a55d1d8
CodyBanks <codydouglasbanks@gmail.com>
2020-05-20 13:50:45 -0400

    Created methods for prompt for string and int

--- Program.cs
+++ Program.cs
@@ -2,11 +2,39 @@
 
 namespace JurrasicPark
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+
+        static string PromptForString(string prompt)
+        {
+            Console.Write(prompt);
+            var inputFromUser = Console.ReadLine();
+
+            return inputFromUser;
+        }
+
+        static int PromptForInteger(string prompt)
+        {
+            Console.Write(prompt);
+            int inputFromUser;
+            var isThisGoodInput = Int32.TryParse(Console.ReadLine(), out inputFromUser);
+
+            if (isThisGoodInput)
+            {
+                return inputFromUser;
+            }
+            else
+            {
+                Console.WriteLine("Sorry, that isn't a valid input, I'm using 0 as your answer.");
+                return 0;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+
+
+
+        }
     }
-  }
 }
