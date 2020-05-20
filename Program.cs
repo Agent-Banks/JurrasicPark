a9d1bf48b1bcd5e5340afa2bc3d6edab1811837b
CodyBanks <codydouglasbanks@gmail.com>
2020-05-20 14:58:14 -0400

    Created Menu for user and finished quit option

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace JurrasicPark
 {
@@ -32,9 +33,65 @@ namespace JurrasicPark
         }
         static void Main(string[] args)
         {
+            var tyrannosaurus = new Dinosaur
+            {
+                Name = "Tyrannosaurus",
+                DietType = "Carnivore",
+                WhenAcquired = "",
+                Weight = "12.5 tons",
+                EnclouserNumber = 10,
+            };
 
+            var triceratops = new Dinosaur
+            {
+                Name = "Triceratops",
+                DietType = "Herbivore",
+                WhenAcquired = "",
+                Weight = "10 tons",
+                EnclouserNumber = 2,
+            };
 
+            var velociraptor = new Dinosaur
+            {
+                Name = "Velociraptor",
+                DietType = "Carnivore",
+                WhenAcquired = "",
+                Weight = "30 pounds",
+                EnclouserNumber = 10,
+            };
 
+            var listOfDinosaurs = new List<Dinosaur>();
+
+            listOfDinosaurs.Add(tyrannosaurus);
+            listOfDinosaurs.Add(triceratops);
+            listOfDinosaurs.Add(velociraptor);
+
+            Console.WriteLine();
+            Console.WriteLine($"Welcome to our Dinosaur database, we currently have {listOfDinosaurs.Count} Dinosaurs in our park.");
+            Console.WriteLine();
+
+            var userWantsToQuit = false;
+
+            while (userWantsToQuit == false)
+            {
+                // Menu is shown to user and they must pick an option
+                Console.WriteLine("Please choose an option");
+                Console.WriteLine("(V)iew all the Dinosaurs in the park");
+                Console.WriteLine("(A)dd a Dinosaur to the database");
+                Console.WriteLine("(R)emove a Dinosaur for the database");
+                Console.WriteLine("(T)ransfer a Dinosaur to a new Enclosure");
+                Console.WriteLine("(S)ummary of all the Carnivores and Herbivores in the park");
+                Console.WriteLine("(Q)uit the application");
+                Console.WriteLine();
+
+                var option = PromptForString("Option: ");
+
+                if (option == "Q")
+                {
+                    userWantsToQuit = true;
+                }
+
+            }
         }
     }
 }
